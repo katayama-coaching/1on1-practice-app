5497e7b6c50fedb8c85c974d11c54cfeb2246f32
カタヤマ <freakers.s.k@gmail.com>
2026-06-25 23:14:10 +0900

    Add missing runtime files for login

---
+++ supabase/config.js
@@ -0,0 +1,5 @@
+window.__APP_CONFIG__ = {
+  supabaseUrl: 'https://phrypbhytfkzwsuasbmc.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBocnlwYmh5dGZrendzdWFzYm1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTg2OTQsImV4cCI6MjA5NzU5NDY5NH0.qWeLxUgUArsjfM6utf41seRWkRwjT4GA8WdEs0GVYYI',
+  supabaseSchema: 'public'
+};
